a394cd83bbd8d12407c8ca74d544bf5a3b0fef3a
NoamDev <noam.yem@gmail.com>
2020-01-02 13:13:43 +0200

    first commit

---
+++ init.sh
@@ -0,0 +1,16 @@
+git clone https://github.com/SoftEtherVPN/SoftEtherVPN_Stable.git
+sudo apt -y install cmake gcc g++ libncurses5-dev libreadline-dev libssl-dev make zlib1g-dev
+./configure
+sudo make install
+sudo vpnserver start
+
+openvpnconfig=mktemp /tmp/openvpnconfig_XXXXXXXXX.zip
+echo "Enter new password:"
+read -s password
+(cat <<EOF
+ServerPasswordSet $password
+OpenVpnEnable yes /PORTS:"1194"
+SecureNatEnable
+OpenVpnMakeConfig $openvpnconfig
+EOF
+)|sudo vpncmd localhost:5555 /server /adminhub:DEFAULT /password: /IN
